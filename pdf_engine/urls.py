cc1f3b873c30a9623a19123e5001ce569d07b6ad
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2022-04-22 02:03:32 +0000

    Add document print out and passport size phorograph print out

--- pdf_engine/urls.py
+++ pdf_engine/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path("controller-form/<int:application_id>/",
          views.GenerateControllerFormView.as_view(),
          name="controller_form"),
+    path("generate-passport-size-photo/<int:application_id>/",
+         views.GeneratePassportSizePhotoView.as_view(),
+         name="generate_passport_size_photo"),
+    path("documents-print-out/<int:application_id>/",
+         views.DocumentsPrintOutView.as_view(),
+         name="documents_print_out"),
 ]
--- pdf_engine/views.py
+++ pdf_engine/views.py
@@ -8,6 +8,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 
 from dashboard.models import Application
+from setup.models import ApplicationDocumentType
 
 from .utils import render_to_pdf
 
@@ -66,6 +67,54 @@ class GenerateControllerFormView(PermissionRequiredMixin, View):
         if pdf:
             response = HttpResponse(pdf, content_type='application/pdf')
             filename = "Controller_Form.pdf"
+            content = f"inline; filename={filename}"
+            download = request.GET.get("download")
+            if download:
+                content = f"attachment; filename={filename}"
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found", status=404)
+
+
+class GeneratePassportSizePhotoView(View):
+    template_name = 'pdf_engine/passport_size_photo.html'
+
+    @method_decorator(login_required(login_url="accounts:login"))
+    def get(self, request, application_id):
+        application = get_object_or_404(Application, id=application_id)
+        context = {
+            "application": application,
+        }
+        pdf = render_to_pdf(self.template_name, context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Photograph.pdf"
+            content = f"inline; filename={filename}"
+            download = request.GET.get("download")
+            if download:
+                content = f"attachment; filename={filename}"
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found", status=404)
+
+
+class DocumentsPrintOutView(View):
+    template_name = 'pdf_engine/documents_print_out.html'
+
+    @method_decorator(login_required(login_url="accounts:login"))
+    def get(self, request, application_id):
+        application = get_object_or_404(Application, id=application_id)
+        document_types = ApplicationDocumentType.objects.all()
+        current_time = datetime.now().strftime("%d %B, %Y")
+        context = {
+            "application": application,
+            "current_time": current_time,
+            "document_types": document_types,
+        }
+        pdf = render_to_pdf(self.template_name, context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Photograph.pdf"
             content = f"inline; filename={filename}"
             download = request.GET.get("download")
             if download:
